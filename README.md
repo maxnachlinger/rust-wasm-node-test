bbdda96c8fdef06113703dff36edde45fcc70d48
m0n0096 <max.nachlinger@walmart.com>
2024-11-10 17:29:24 -0800

    chore: bump deps, optimize wasm, use pnpm

--- README.md
+++ README.md
@@ -6,45 +6,43 @@
 
 - [Install Rust](https://www.rust-lang.org/tools/install)
 - get `wasm-pack`: `curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh`
-- install NPM deps: `npm i`
+- install npm deps: `pnpm i`
+- install `binaryen` - `brew install binaryen`
 
 ## Build
+
 ```shell
-npm run build
-# Or
-# wasm-pack build --release --target nodejs --out-dir dist --out-name index
+pnpm build
 ```
 
 ## Run Rust tests
+
 ```shell
 wasm-pack test --node
 ```
 
-## Run Rust linting
-```shell
-cargo clippy
-```
-
 ## Benchmark:
+
 ```shell
-npm run bench
+pnpm bench
 ```
 
 ## Results on my machine
 
 ```shell
 node -v
-v22.7.0
+v23.1.0
 ```
 
 ### Fibonacci Benchmarks
-| | ops/sec | # of runs |
-| :---------- | ----------: | ----------: |
-| js fib 10 | 87,600,274 | 90 |
-| wasm fib 10 | 64,549,231 | 90 |
-| js fib 20 | 57,405,333 | 90 |
-| wasm fib 20 | 62,235,190 | 94 |
-| js fib 40 | 33,332,859 | 98 |
-| wasm fib 40 | 54,507,955 | 94 |
-| js fib 100 | 13,855,898 | 98 |
-| wasm fib 100 | 32,585,150 | 95 |
+
+|              |    ops/sec | # of runs |
+|:-------------|-----------:|----------:|
+| js fib 10    | 85,885,582 |        86 |
+| wasm fib 10  | 16,031,028 |        96 |
+| js fib 40    | 32,857,444 |        95 |
+| wasm fib 40  | 14,402,679 |        98 |
+| js fib 50    | 17,067,576 |        94 |
+| wasm fib 50  | 12,867,424 |        98 |
+| js fib 100   |  8,229,533 |        90 |
+| wasm fib 100 | 10,872,934 |        97 |
--- benchmarks/fibonacci.js
+++ benchmarks/fibonacci.js
@@ -5,55 +5,55 @@ const {jsFibonacci} = require('./js-fibonacci.js');
 const {fibonacci} = require('../dist');
 
 const suite = Benchmark.Suite({
-  onError (...args) {
-    console.log('ERROR!', ...args);
-  },
+    onError(...args) {
+        console.log('ERROR!', ...args);
+    },
 });
 
 const results = [];
 
 suite
     .add('js fib 10', function () {
-      jsFibonacci(10);
+        jsFibonacci(10);
     })
     .add('wasm fib 10', function () {
-      fibonacci(10);
+        fibonacci(10);
     })
-  .add('js fib 20', function () {
-    jsFibonacci(20);
-  })
-  .add('wasm fib 20', function () {
-    fibonacci(20);
-  })
-  .add('js fib 40', function () {
-    jsFibonacci(40);
-  })
-  .add('wasm fib 40', function () {
-    fibonacci(40);
-  })
-  .add('js fib 100', function () {
-    jsFibonacci(100);
-  })
-  .add('wasm fib 100', function () {
-    fibonacci(100);
-  })
-  .on('cycle', function (event) {
-    results.push(event.target);
-  })
-  .on('complete', function () {
-    console.log(`### Fibonacci Benchmarks
+    .add('js fib 40', function () {
+        jsFibonacci(40);
+    })
+    .add('wasm fib 40', function () {
+        fibonacci(40);
+    })
+    .add('js fib 50', function () {
+        jsFibonacci(50);
+    })
+    .add('wasm fib 50', function () {
+        fibonacci(50);
+    })
+    .add('js fib 100', function () {
+        jsFibonacci(100);
+    })
+    .add('wasm fib 100', function () {
+        fibonacci(100);
+    })
+    .on('cycle', function (event) {
+        results.push(event.target);
+    })
+    .on('complete', function () {
+        console.log(`### Fibonacci Benchmarks
 | | ops/sec | # of runs |
 | :---------- | ----------: | ----------: |`);
 
-    results.forEach((result) => {
-      const {
-        name,
-        hz,
-        stats: {sample},
-      } = result;
-      const ops = Math.round(hz).toLocaleString('en-US');
-      const runs = sample.length;
-      console.log(`| ${name} | ${ops} | ${runs} |`);
-    });
-  })
-  .run({async: true});
+        results.forEach((result) => {
+            const {
+                name,
+                hz,
+                stats: {sample},
+            } = result;
+            const ops = Math.round(hz).toLocaleString('en-US');
+            const runs = sample.length;
+            console.log(`| ${name} | ${ops} | ${runs} |`);
+        });
+    })
+    .run({async: true});
